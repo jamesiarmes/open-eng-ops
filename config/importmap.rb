771d253e9f28bdfca8d1fb74ad4722c25104ea5a
James I. Armes <jamesiarmes@gmail.com>
2023-09-24 13:30:10 -0400

    Moved material bootstrap javascript to a more appropriate named directory.
    TODO: Can we avoid committing this altogether by using importmaps or npm?

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,9 +7,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'popper', to: 'popper.js', preload: true
-pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'mdb', to: 'mdb/mdb'
+pin 'mdb-ui-kit', to: 'mdb-ui-kit/mdb', preload: true
 pin 'detect-autofill', to: 'https://ga.jspm.io/npm:detect-autofill@1.1.4/dist/detect-autofill.js'
 pin 'chartkick', to: 'chartkick.js'
 pin 'Chart.bundle', to: 'Chart.bundle.js'
