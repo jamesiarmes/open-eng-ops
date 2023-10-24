cf6a26b83c6696351be711194144ff7748520e31
James I. Armes <jamesiarmes@gmail.com>
2023-10-23 22:57:44 -0400

    Allow pagination to be rendered with a table.

--- app/components/table/component.rb
+++ app/components/table/component.rb
@@ -4,6 +4,7 @@
 #
 # TODO: Add filters.
 class Table::Component < ApplicationComponent
+  renders_one :pagination, Pagination::Component
   renders_many :headers, Table::Header
   renders_many :rows, Table::Row
 end
